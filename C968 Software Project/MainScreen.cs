fb59640206d5f734b8dc7721c6427c52b808e76b
Trey Hall <linkin562@gmail.com>
2022-07-23 02:15:37 -0400

    initial commit

---
+++ C968 Software Project/MainScreen.cs
@@ -0,0 +1,10 @@
+namespace C968_Software_Project
+{
+    public partial class MainScreen : Form
+    {
+        public MainScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
